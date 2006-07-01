4b126dfe078c98a36b0c01b07e49b74ccc19b7e5
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-07-01 03:32:22 +0000

    Import of MIYAGAWA/Geo-Coder-Google-0.02 from CPAN.
    gitpan-cpan-distribution: Geo-Coder-Google
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MIYAGAWA/Geo-Coder-Google-0.02.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- lib/Geo/Coder/Google.pm
+++ lib/Geo/Coder/Google.pm
@@ -1,226 +1,159 @@
 package Geo::Coder::Google;
 
 use strict;
-use warnings;
+our $VERSION = '0.02';
 
-use Geo::Coder::Google::Address;
+use Carp;
+use Encode;
+use JSON::Syck;
+use HTTP::Request;
 use LWP::UserAgent;
-use XML::Simple;
-#use JSON;
 use URI;
-use Data::Dumper;
 
-our $VERSION = '0.01';
-use constant DEBUG => 0;
-use constant GEO_HOST => qq|http://maps.google.com/maps/geo|;
+sub new {
+    my($class, %param) = @_;
 
-my %ERROR_CODES = (
-    601 => "G_GEO_MISSING_ADDRESS: No address specified",
-    602 => "G_GEO_UNKNOWN_ADDRESS: The address entered cannot be identified",
-    603 => "G_GEO_UNAVAILABLE_ADDRESS: There is no geocoding information for this address",
-    610 => "G_GEO_BAD_KEY: The API KEY supplied is invalid",
-    620 => "G_GEO_TOO_MANY_QUERIES: This client has exceeded the query count.  Try later.",
-    500 => "G_GEO_SERVER_ERROR: Internal Geocode Service Error, Try later.",
-);
+    my $key = delete $param{apikey}
+        or Carp::croak("Usage: new(apikey => \$apikey)");
+
+    my $ua   = delete $param{ua}   || LWP::UserAgent->new(agent => __PACKAGE__ . "/$VERSION");
+    my $host = delete $param{host} || 'maps.google.com';
+
+    bless { key => $key, ua => $ua, host => $host }, $class;
+}
+
+sub geocode {
+    my $self = shift;
+
+    my %param;
+    if (@_ % 2 == 0) {
+        %param = @_;
+    } else {
+        $param{location} = shift;
+    }
+
+    my $location = $param{location}
+        or Carp::croak("Usage: geocode(location => \$location)");
+
+    if (Encode::is_utf8($location)) {
+        $location = Encode::encode_utf8($location);
+    }
+
+    my $uri = URI->new("http://$self->{host}/maps/geo");
+    $uri->query_form(q => $location, output => 'json', key => $self->{key});
+
+    my $res = $self->{ua}->get($uri);
+
+    if ($res->is_error) {
+        Carp::croak("Google Maps API returned error: " . $res->status_line);
+    }
+
+    # Ugh, Google Maps returns so stupid HTTP header
+    # Content-Type: text/javascript; charset=UTF-8; charset=Shift_JIS
+    my @ctype = $res->content_type;
+    my $charset = ($ctype[1] =~ /charset=([\w\-]+)$/)[0] || "utf-8";
+
+    my $content = Encode::decode($charset, $res->content);
+
+    local $JSON::Syck::ImplicitUnicode = 1;
+    my $data = JSON::Syck::Load($content);
+
+    my @placemark = @{ $data->{Placemark} || [] };
+    wantarray ? @placemark : $placemark[0];
+}
+
+1;
+__END__
 
 =head1 NAME
 
-Geo::Coder::Google - Geocoder that utilizes the public Google Geocoder API
+Geo::Coder::Google - Google Maps Geocoding API
 
 =head1 SYNOPSIS
 
-    use Geo::Coder::Google;
-    use Data::Dumper;
+  use Geo::Coder::Google;
 
-    my $geocoder = Geo::Coder::Google->new({ key => "$API_KEY" });
-
-    my $address  = '1600 Pennsylvania Ave Washington DC';
-    
-    # Returns an array of matching records.  Geocoding is often not precise
-    # enough to guarantee one record, so this is always an array
-    my @response = $geocoder->geocode($address);
-
-    foreach my $addr ( @response ) {
-        print $addr->address, "\n"; # Formatted address (looks pretty)
-        print "This address is at: ",
-            $addr->latitude, " x ", $addr->longitude, "\n";
-        # Now for the other fields.  Because Geocoding isn't *just* in the
-        # US, the format is loosely based upon xAL.  You can readup on it
-        # here: http://www.oasis-open.org/committees/ciq/ciq.html#6
-        print $addr->administrativearea, "\n";    # State/Province (e.g., "CA")
-        print $addr->subadministrativearea, "\n"; # County (e.g., "Santa Clara")
-        print $addr->locality, "\n";     # The City
-        print $addr->postalcode, "\n";   # Postal Code
-        print $addr->thoroughfare, "\n"; # Just the number and street info
-    }
+  my $geocoder = Geo::Coder::Google->new(apikey => 'Your API Key');
+  my $location = $geocoder->geocode( location => 'Hollywood and Highland, Los Angeles, CA' );
 
 =head1 DESCRIPTION
 
-This module utilizes LWP::UserAgent to request geocode results from the Google
-Geocode Service.  This requires that you register for an API key at:
-
-http://www.google.com/apis/maps/signup.html
-
-Once you have your key, you can begin using this module for geocoding.  If
-your key is invalid, or misentered, you will receive an "Error 610".  
-
-=head2 ERROR CASES
-
-=over
-
-=item G_GEO_SUCCESS (200)
-
-Successful geocode
-
-=item G_GEO_MISSING_ADDRESS (601)
-
-You have to supply at least some form of an address.
-
-=item G_GEO_UNKNOWN_ADDRESS (602)
-
-You probably supplied gibberish, didn't you?  Didn't you!?
-
-=item G_GEO_UNAVAILABLE_ADDRESS (603)
-
-Google knows your address.  But it is a secret.  Sshh.
-
-=item G_GEO_BAD_KEY (610)
-
-Your key is not valid.  Check it again.
-
-=item G_GEO_TOO_MANY_QUERIES (620)
-
-You have used up too many queries.  There is a 50,000 limit that is likely to
-change (who knows which direction).
-
-=item G_GEO_SERVER_ERROR (500)
-
-Server Error.  Oops!
-
-=back
+Geo::Coder::Google provides a geocoding functionality using Google Maps API.
 
 =head1 METHODS
 
-=head2 new
+=over 4
 
-This simple constructor gets you started.
-    
-    my $geocoder = Geo::Coder::Google->new({ key => $YOURKEY });
+=item new
 
-Parameters:
+  $geocoder = Geo::Coder::Google->new(apikey => 'Your API Key');
+  $geocoder = Geo::Coder::Google->new(apikey => 'Your API Key', host => 'maps.google.co.jp');
 
-=over
+Creates a new geocoding object. You should pass a valid Google Maps
+API Key as C<apikey> parameter.
 
-=item key
+When you'd like to query Japanese address, you might want to set
+I<host> parameter, which should point to I<maps.google.co.jp>. I think
+this also applies to other countries like UK (maps.google.co.uk), but
+so far I only tested with I<.com> and I<.co.jp>.
 
-Required.  Set this to your Google API Key
+=item geocode
 
-=item timeout
+  $location = $geocoder->geocode(location => $location);
+  @location = $geocoder->geocode(location => $location);
 
-Optional timeout to use before giving up and reporting failure.
+Queries I<$location> to Google Maps geocoding API and returns hash
+refernece returned back from API server. When you cann the method in
+an array context, it returns all the candidates got back, while it
+returns the 1st one in a scalar context.
 
-=item agent
+When you'd like to pass non-ascii string as a location, you should
+pass it as either UTF-8 bytes or Unicode flagged string.
 
-Optional parameter to set the User Agent.  Defaults to Geo::Coder::Google/$VERSION
+Returned data structure is as follows:
+
+  {
+    'AddressDetails' => {
+      'Country' => {
+        'AdministrativeArea' => {
+          'SubAdministrativeArea' => {
+            'SubAdministrativeAreaName' => 'San Francisco',
+            'Locality' => {
+              'PostalCode' => {
+                'PostalCodeNumber' => '94107'
+              },
+              'LocalityName' => 'San Francisco',
+              'Thoroughfare' => {
+                'ThoroughfareName' => '548 4th St'
+              }
+            }
+          },
+          'AdministrativeAreaName' => 'CA'
+        },
+        'CountryNameCode' => 'US'
+      }
+    },
+    'address' => '548 4th St, San Francisco, CA 94107, USA',
+    'Point' => {
+      'coordinates' => [
+        '-122.397323',
+        '37.778993',
+        0
+      ]
+    }
+  }
 
 =back
 
-=cut
+=head1 AUTHOR
 
-sub new {
-    my $class  = shift;
-    my $attr   = ( $_[0] and ref $_[0] eq 'HASH' ) ? $_[0] : undef;
-    my %config = map { uc($_) => $attr->{$_} } keys %$attr;
-    unless ( $config{KEY} ) {
-        if ( $ENV{GMAP_KEY} ) {
-            $config{KEY} = $ENV{GMAP_KEY};
-        } else {
-            warn "Unable to use Geo::Coder::Google without an API key.\n";
-            return undef;
-        }
-    }
-    my $self = {
-        _ua => LWP::UserAgent->new(
-            agent => $config{AGENT} || "Geo::Coder::Google/$VERSION"
-        ),
-        key => $config{KEY}
-    };
+Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
 
-    if ( $config{TIMEOUT} and int($config{TIMEOUT}) > 0 ) {
-        $self->{_ua}->timeout(int($config{TIMEOUT}));
-    }
-    bless $self, $class;
-}
-
-=head2 geocode
-
-This method returns an array of the results for the address specified.
-
-=cut
-
-sub geocode {
-    my ( $self, $address, $format ) = @_;
-    return () unless $address;
-
-    my $uri = URI->new(+GEO_HOST, 'http');
-    $format = lc($format);
-    $format = 'xml' unless ( $format =~ /^(xml|kml|json)$/ );
-    $uri->query_form(
-        key    => $self->{key},
-        output => $format,
-        q      => $address
-    );
-    #warn "Fetching: " . $uri->as_string . "\n";
-    my $res     = $self->{_ua}->get( $uri->as_string );
-    my @result  = ();
-    if ( $res->is_success ) {
-        # JSON support coming soon
-        if ( $format eq 'json' ) {
-            #my $json = new JSON(skipinvalid => 1);
-            #print $res->content;
-            #$result = $json->jsonToObj($res->content);
-        }
-        elsif ( $format eq 'xml' ) {
-            #print $res->content;
-            my $res = XMLin($res->content, ForceArray => 1);
-            if ( $res->{Response} and
-                 $res->{Response}->[0]->{Status}->[0]->{code}->[0] eq '200' )
-            {
-                foreach my $address ( @{$res->{Response}->[0]->{Placemark}} ) {
-                    push @result,
-                        Geo::Coder::Google::Address->new_xml($address);
-                }
-            }
-            elsif ( $res->{Response}->[0]->{Status}->[0]->{code}->[0] ) {
-                warn "Geocode error: " .
-                    $ERROR_CODES{$res->{Response}->[0]->{Status}->[0]->{code}->[0]} . "\n";
-            }
-        } else {
-            return $res->content;
-        }
-    } else {
-        warn "Failed fetching response from " .
-            +GEO_HOST . ": " . $res->status_line;
-    }
-    return @result;
-}
-
-=head1 AUTHORS
-
-Copyright 2006 J. Shirley <jshirley@gmail.com>
-
-This program is free software;  you can redistribute it and/or modify it under
-the same terms as Perl itself.  That means either (a) the GNU General Public
-License or (b) the Artistic License.
-
-=head2 THANKS
-
-Google!  You can do no evil, except in China.  We still love you, though.
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-L<Geo::Coder::Google::Address> - The Google address object
+L<Geo::Coder::Yahoo>, L<http://www.google.com/apis/maps/documentation/#Geocoding_Examples>
 
 =cut
-
-1;
